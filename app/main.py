963c786a319dc1dca820b3c5c2301bd8e57dbf85
Miikael Volkonski <miikael@volkonski.com>
2026-03-17 19:11:27 +0200

    Set up frontend scaffold

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-from pyicloud import PyiCloudService
+# from pyicloud import PyiCloudService
+import webview
+
 
 def main():
+    webview.create_window('Hello world', 'https://linkedin.com')
+    webview.start()
     print("Project started successfully.")
     print("pyicloud import works.")
 
 if __name__ == "__main__":
     main()
-    
