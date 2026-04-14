fe83bee2da55d7fbbfa0e6e837e386740a5635d0
Miikael Volkonski <miikael@volkonski.com>
2026-04-14 12:52:29 +0300

    update main.js & reamde

--- app/ui/js/main.js
+++ app/ui/js/main.js
@@ -1,7 +1,8 @@
 import { login, submit2FA, restartLogin } from './login.js';
 import { loadAlbums, startSort, cancelSort } from './albums.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+// document.addEventListener('DOMContentLoaded', () => {
   const loginBtn = document.getElementById('loginBtn');
   const verifyBtn = document.getElementById('verifyBtn');
   const restartBtn = document.getElementById('restartBtn');
@@ -18,16 +19,3 @@ document.addEventListener('DOMContentLoaded', () => {
 window.addEventListener('pywebviewready', async () => {
   await loadAlbums();
 });
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const loginBtn = document.getElementById('loginBtn');
-//   const verifyBtn = document.getElementById('verifyBtn');
-//   const restartBtn = document.getElementById('restartBtn');
-//   const startButton = document.getElementById('download-btn');
-
-//   loginBtn.addEventListener('click', login);
-//   verifyBtn.addEventListener('click', submit2FA);
-//   restartBtn.addEventListener('click', restartLogin);
-//   startButton.addEventListener('click', startSort)
-// });
